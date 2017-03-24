c25b32876f4d72470e51ce76e9ebdf939a12d19e
René Fleschenberg <rene@fleschenberg.net>
2017-03-24 22:37:10 +0100

    Stub serializer classes

--- src/rest_checkout/serializers.py
+++ src/rest_checkout/serializers.py
@@ -3,3 +3,11 @@ from rest_framework.serializers import Serializer
 
 class CheckoutSerializer(Serializer):
     pass
+
+
+class ShippingAddressSerializer(Serializer):
+    pass
+
+
+class BillingAddressSerializer(Serializer):
+    pass
