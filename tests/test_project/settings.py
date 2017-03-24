016a13f85d7b8bb806291ab2c9bb7e1695b58c94
René Fleschenberg <rene@fleschenberg.net>
2017-03-25 00:31:06 +0100

    A simple test with DB access

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -1,10 +1,12 @@
 SECRET_KEY = 'notsecret'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
     }
 }
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -15,10 +17,40 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'menus',
+    'djangocms_text_ckeditor',
+    'treebeard',
+    'cms',
+    'filer',
     'easy_thumbnails',
     'shop',
+    'post_office',
+    'cmsplugin_cascade',
     'rest_checkout',
     'test_project',
 ]
 
+SITE_ID = 1
 SHOP_APP_LABEL = 'test_project'
+LANGUAGE_CODE = 'en'
+LANGUAGES = [
+    ('en', "English"),
+    ('de', "German"),
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'sekizai.context_processors.sekizai',
+                'cms.context_processors.cms_settings',
+            ],
+        },
+    },
+]
--- tests/test_rest_checkout.py
+++ tests/test_rest_checkout.py
@@ -1,8 +1,21 @@
+from django.contrib.auth import get_user_model
+
+import pytest
+
 import test_project.serializers
-from test_project.models import ShippingAddress
+from test_project.models import Customer, ShippingAddress
 
 
-def test_shipping_address_serializer():
-    address = ShippingAddress(name='René')
+@pytest.fixture
+def customer():
+    User = get_user_model()
+    user = User.objects.create(username='rene')
+    customer = Customer.objects.create(user=user)
+    return customer
+
+
+@pytest.mark.django_db
+def test_shipping_address_serializer(customer):
+    address = ShippingAddress.objects.create(name='René', customer=customer)
     serializer = test_project.serializers.ShippingAddressSerializer(address)
     assert serializer.data['name'] == 'René'
