490dae92f0aab0166fe2ef0e53fe8345f922d814
René Fleschenberg <rene@fleschenberg.net>
2017-03-24 23:06:49 +0100

    isort

--- tests/test_project/models.py
+++ tests/test_project/models.py
@@ -1,3 +1,4 @@
-from shop.models.defaults.address import ShippingAddress, BillingAddress  # noqa
+from shop.models.defaults.address import BillingAddress  # noqa
+from shop.models.defaults.address import ShippingAddress
 from shop.models.defaults.customer import Customer  # noqa
 from shop.models.defaults.order import Order  # noqa
